05b20b6ed7ad9ba95380ca530b7d9ddf51d361d2
Georgy Kcojev <kcojev@gmail.com>
2023-01-24 16:56:00 +0300

    customize linksContainer to links height

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,19 @@ import logo from './logo.svg'
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+
+  useEffect(() => {
+    const linksHeigth = linksRef.current.getBoundingClientRect().height
+    if(showLinks) {
+      linksContainerRef.current.style.height = `${linksHeigth}px`
+    }
+    else {
+      linksContainerRef.current.style.height = '0px'
+    }
+  }, [showLinks]) 
+  
   return (
     <nav>
       <div className="nav-center">
@@ -15,8 +28,8 @@ const Navbar = () => {
           </button>
         </div>
 
-        <div className={`${showLinks ? 'show-container links-container' : 'show-container'}`}>
-          <ul className="links">
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const {id, url, text} = link
               return (
